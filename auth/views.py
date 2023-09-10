a92e0d71c7cf24a2dae3d9bceda69ea993617f9d
Aayush3014 <ayushak3008@gmail.com>
2023-09-10 14:31:35 +0530

    Adding Authentication Feature

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def handlelogin(request):
+    return render(request, "authentication/login.html")
+
+def logout(request):
+    return redirect("/auth/login")
