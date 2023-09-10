ff8dcf7621132f3d9f3da80e13414dcd138ebd65
Aayush3014 <ayushak3008@gmail.com>
2023-09-10 17:42:15 +0530

    Adding Signup function

--- ShopAuth/views.py
+++ ShopAuth/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render,redirect
-
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def signup(request):
-    return render(request, "signup.html")
+
+
+    if request.method=="POST":
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+
+        if password != confirm_password:
+            return HttpResponse("Password did not match.")
+            # return render(request, "signup.html")
+        
+        try:
+            if User.objects.get(username = email):
+                return HttpResponse("Email already exists.")
+                # return render(request, 'signup.html')
+        except Exception as e:
+            pass
+        
+        user = User.objects.create_user(email,email,password)
+        user.save()
+        return HttpResponse("user is created.",email)
+    return render(request, 'signup.html')
+    
+
 
 def handlelogin(request):
     return render(request, "login.html")
