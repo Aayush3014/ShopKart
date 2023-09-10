de430cd1c7b1f673d475ad58eecf94642a481e93
Aayush3014 <ayushak3008@gmail.com>
2023-09-10 21:26:41 +0530

    Email configuration for activating account

--- Shoppy/settings.py
+++ Shoppy/settings.py
@@ -131,3 +131,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Sending Emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'axxcelera2000@gmail.com'
+EMAIL_HOST_PASSWORD = ''
