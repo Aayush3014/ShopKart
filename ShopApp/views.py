0175a6d397efb647826bce71f638c767a42d05c7
Aayush3014 <ayushak3008@gmail.com>
2023-09-16 01:18:02 +0530

    Added Checkout Function

--- ShopApp/views.py
+++ ShopApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from ShopApp.models import Contact, Product
 from django.contrib import messages
 from math import ceil
@@ -94,5 +94,35 @@ def contact(request):
     return render(request, 'contact.html')
 
 
+
+
+
 def about(request):
     return render(request, 'about.html')
+
+
+
+
+def checkout(request):
+    def checkout(request):
+        if not request.user.is_authenticated:
+            messages.warning(request,"Login & Try Again")
+            return redirect('/auth/login')
+
+        if request.method=="POST":
+            items_json = request.POST.get('itemsJson', '')
+            name = request.POST.get('name', '')
+            amount = request.POST.get('amt')
+            email = request.POST.get('email', '')
+            address1 = request.POST.get('address1', '')
+            address2 = request.POST.get('address2','')
+            city = request.POST.get('city', '')
+            state = request.POST.get('state', '')
+            zip_code = request.POST.get('zip_code', '')
+            phone = request.POST.get('phone', '')
+            Order = Orders(items_json=items_json,name=name,amount=amount, email=email, address1=address1,address2=address2,city=city,state=state,zip_code=zip_code,phone=phone)
+            print(amount)
+            Order.save()
+            update = OrderUpdate(order_id=Order.order_id,update_desc="the order has been placed")
+            update.save()
+            thank = True
