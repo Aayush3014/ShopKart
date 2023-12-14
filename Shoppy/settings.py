09d926385fd6b567fe7c9c83a10759b8f0ed2eb8
Aayush3014 <ayushak3008@gmail.com>
2023-12-14 17:41:54 +0530

    Configure Mysql Installation error

--- Shoppy/settings.py
+++ Shoppy/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'Shoppy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'shopkartdatabase',
+		'USER': 'root',
+		'PASSWORD': 'pass',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
